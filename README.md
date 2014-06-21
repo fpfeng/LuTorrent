8319c52de87906ef3361173c506f5f77fe6ef779
feng <feng@box.local>
2014-06-21 23:50:22 +0800

    update

--- README.md
+++ README.md
@@ -1,11 +1,11 @@
-##Change torrent file to random name and remove useless info, cheat on baiduyun. So we can keep going lu.
+Change torrent file to random name and remove useless info, cheat on baiduyun. So we can keep going lu.
 
-###How to:
+How to:
 
 `python lutorrent.py av.torrent`
 
-###Upload `new_av.torrent` to baiduyun, it should pass this time.
+Upload `new_av.torrent` to baiduyun, it should pass this time.
 
-##`bencode.py` and `BTL.py` are copy from [bencode](https://pypi.python.org/pypi/bencode), feel free to delete these two files after install it:
+`bencode.py` and `BTL.py` are copy from [bencode](https://pypi.python.org/pypi/bencode), feel free to delete these two files after install it:
 
 `pip install bencode`
---
+++ lutorrent.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+import bencode
+import random
+import string
+import sys
+from os import path
+from bencode import BTFailure
+
+
+def random_string():
+    r = ''.join(
+        random.choice(
+            string.ascii_uppercase +
+            string.digits) for x in range(6))
+    return r
+
+
+def trash_useless(info):  # dump useless info
+    keys = [
+        'publisher-url',
+        'publisher',
+        'publisher-url.utf-8',
+        'publisher.utf-8',
+        'name.utf-8']
+    for k in keys:
+        if k in info:
+            info.pop(k)
+    return info
+
+# differents see
+# https://wiki.theory.org/BitTorrentSpecification#Info_Dictionary
+
+
+def single_file(info):
+    dot_index = info['name'].rfind(".")  # find the last . in the file
+    if dot_index != -1:
+        # random file name
+        info['name'] = random_string() + info['name'][dot_index:]
+
+
+def multi_file(info):
+    info['name'] = random_string()
+    for i in info['files']:
+        if 'path' in i:
+            i['path'] = short_and_random(i)
+        if 'path.utf-8' in i:
+            i.pop('path.utf-8')
+
+
+def short_and_random(item):
+    # the file is the last one of a path list
+    dot_index = item['path'][-1].rfind(".")
+    if dot_index != -1:
+        # no more folder just file
+        return [random_string() + item['path'][-1][dot_index:]]
+
+
+def lu_torrent(torrent):
+    try:
+        decoded_data = bencode.bdecode(torrent.read())
+        old_info = decoded_data['info']
+    except BTFailure:
+        print "error, not a valid torrent."
+    else:
+        if 'files' in old_info:
+            multi_file(old_info)
+        else:
+            single_file(old_info)
+        decoded_data['info'] = trash_useless(
+            old_info)  # last step change to new info
+        new_torrent = open("new_" + path.basename(sys.argv[1]), "w")
+        new_torrent.write(bencode.bencode(decoded_data))
+        new_torrent.close()
+        torrent.close()
+
+if __name__ == '__main__':
+    try:
+        old_torrent = open(sys.argv[1], "r")
+    except IOError:
+        print "error, can not open torrent file."
+    else:
+        lu_torrent(old_torrent)
