365ab057634b80dd2511c6c4a443da6c75211a1b
smoke <187j3x1@gmail.com>
2014-06-22 12:58:20 +0800

    fix read and write problem on windows
    now it is in binary mode

--- lutorrent.py
+++ lutorrent.py
@@ -16,17 +16,19 @@ def random_string():
     return r
 
 
-def trash_useless(info):  # dump useless info
+def trash_useless(data):  # dump useless info
     keys = [
         'publisher-url',
         'publisher',
         'publisher-url.utf-8',
         'publisher.utf-8',
-        'name.utf-8']
+        'name.utf-8',]
     for k in keys:
-        if k in info:
-            info.pop(k)
-    return info
+        if k in data['info']:
+            data['info'].pop(k)
+    if 'comment' in data:
+        data.pop('comment')
+    return data
 
 # differents see
 # https://wiki.theory.org/BitTorrentSpecification#Info_Dictionary
@@ -37,6 +39,7 @@ def single_file(info):
     if dot_index != -1:
         # random file name
         info['name'] = random_string() + info['name'][dot_index:]
+    return info
 
 
 def multi_file(info):
@@ -46,6 +49,7 @@ def multi_file(info):
             i['path'] = short_and_random(i)
         if 'path.utf-8' in i:
             i.pop('path.utf-8')
+    return info
 
 
 def short_and_random(item):
@@ -64,20 +68,18 @@ def lu_torrent(torrent):
         print "error, not a valid torrent."
     else:
         if 'files' in old_info:
-            multi_file(old_info)
+            decoded_data['info'] = multi_file(old_info)
         else:
-            single_file(old_info)
-        decoded_data['info'] = trash_useless(
-            old_info)  # last step change to new info
+            decoded_data['info'] = single_file(old_info)
         split_path = path.split(path.abspath(sys.argv[1]))
-        new_torrent = open(path.join(split_path[0], 'new_' + split_path[1]), "w")
-        new_torrent.write(bencode.bencode(decoded_data))
+        new_torrent = open(path.join(split_path[0], 'new_' + split_path[1]), "wb")
+        new_torrent.write(bencode.bencode(trash_useless(decoded_data)))
         new_torrent.close()
         torrent.close()
 
 if __name__ == '__main__':
     try:
-        old_torrent = open(sys.argv[1], "r")
+        old_torrent = open(sys.argv[1], "rb")
     except IOError:
         print "error, can not open torrent file."
     else:
